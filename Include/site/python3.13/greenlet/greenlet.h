df879adf7c1896c34a8fa68c489ee100900da23f
shanjay <shanjayakasht@gmail.com>
2025-12-04 15:49:57 +0530

    Remove deprecated comments and clean up header

--- Include/site/python3.13/greenlet/greenlet.h
+++ Include/site/python3.13/greenlet/greenlet.h
@@ -1,7 +1,3 @@
-/* -*- indent-tabs-mode: nil; tab-width: 4; -*- */
-
-/* Greenlet object interface */
-
 #ifndef Py_GREENLETOBJECT_H
 #define Py_GREENLETOBJECT_H
 
@@ -12,7 +8,7 @@
 extern "C" {
 #endif
 
-/* This is deprecated and undocumented. It does not change. */
+
 #define GREENLET_VERSION "1.0.0"
 
 #ifndef GREENLET_MODULE
@@ -29,9 +25,6 @@ typedef struct _greenlet {
 #define PyGreenlet_Check(op) (op && PyObject_TypeCheck(op, &PyGreenlet_Type))
 
 
-/* C API functions */
-
-/* Total number of symbols that are exported */
 #define PyGreenlet_API_pointers 12
 
 #define PyGreenlet_Type_NUM 0
@@ -50,7 +43,7 @@ typedef struct _greenlet {
 #define PyGreenlet_GET_PARENT_NUM 11
 
 #ifndef GREENLET_MODULE
-/* This section is used by modules that uses the greenlet C API */
+
 static void** _PyGreenlet_API = NULL;
 
 #    define PyGreenlet_Type \
@@ -62,32 +55,16 @@ static void** _PyGreenlet_API = NULL;
 #    define PyExc_GreenletExit \
         ((PyObject*)_PyGreenlet_API[PyExc_GreenletExit_NUM])
 
-/*
- * PyGreenlet_New(PyObject *args)
- *
- * greenlet.greenlet(run, parent=None)
- */
+
 #    define PyGreenlet_New                                        \
         (*(PyGreenlet * (*)(PyObject * run, PyGreenlet * parent)) \
              _PyGreenlet_API[PyGreenlet_New_NUM])
 
-/*
- * PyGreenlet_GetCurrent(void)
- *
- * greenlet.getcurrent()
- */
+
 #    define PyGreenlet_GetCurrent \
         (*(PyGreenlet * (*)(void)) _PyGreenlet_API[PyGreenlet_GetCurrent_NUM])
 
-/*
- * PyGreenlet_Throw(
- *         PyGreenlet *greenlet,
- *         PyObject *typ,
- *         PyObject *val,
- *         PyObject *tb)
- *
- * g.throw(...)
- */
+
 #    define PyGreenlet_Throw                 \
         (*(PyObject * (*)(PyGreenlet * self, \
                           PyObject * typ,    \
@@ -95,41 +72,23 @@ static void** _PyGreenlet_API = NULL;
                           PyObject * tb))    \
              _PyGreenlet_API[PyGreenlet_Throw_NUM])
 
-/*
- * PyGreenlet_Switch(PyGreenlet *greenlet, PyObject *args)
- *
- * g.switch(*args, **kwargs)
- */
+
 #    define PyGreenlet_Switch                                              \
         (*(PyObject *                                                      \
            (*)(PyGreenlet * greenlet, PyObject * args, PyObject * kwargs)) \
              _PyGreenlet_API[PyGreenlet_Switch_NUM])
 
-/*
- * PyGreenlet_SetParent(PyObject *greenlet, PyObject *new_parent)
- *
- * g.parent = new_parent
- */
+
 #    define PyGreenlet_SetParent                                 \
         (*(int (*)(PyGreenlet * greenlet, PyGreenlet * nparent)) \
              _PyGreenlet_API[PyGreenlet_SetParent_NUM])
 
-/*
- * PyGreenlet_GetParent(PyObject* greenlet)
- *
- * return greenlet.parent;
- *
- * This could return NULL even if there is no exception active.
- * If it does not return NULL, you are responsible for decrementing the
- * reference count.
- */
+
 #     define PyGreenlet_GetParent                                    \
     (*(PyGreenlet* (*)(PyGreenlet*))                                 \
      _PyGreenlet_API[PyGreenlet_GET_PARENT_NUM])
 
-/*
- * deprecated, undocumented alias.
- */
+
 #     define PyGreenlet_GET_PARENT PyGreenlet_GetParent
 
 #     define PyGreenlet_MAIN                                         \
@@ -147,18 +106,15 @@ static void** _PyGreenlet_API = NULL;
 
 
 
-/* Macro that imports greenlet and initializes C API */
-/* NOTE: This has actually moved to ``greenlet._greenlet._C_API``, but we
-   keep the older definition to be sure older code that might have a copy of
-   the header still works. */
+
 #    define PyGreenlet_Import()                                               \
         {                                                                     \
             _PyGreenlet_API = (void**)PyCapsule_Import("greenlet._C_API", 0); \
         }
 
-#endif /* GREENLET_MODULE */
+#endif
 
 #ifdef __cplusplus
 }
 #endif
-#endif /* !Py_GREENLETOBJECT_H */
+#endif 
